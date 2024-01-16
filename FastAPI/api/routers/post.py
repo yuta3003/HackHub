4565cc782e5279d7b444d9ee5815c3714c50d972
yuta3003 <y.nakazawa3003+github@gmail.com>
2024-01-16 17:29:28 +0900

    update

--- FastAPI/api/routers/post.py
+++ FastAPI/api/routers/post.py
@@ -9,18 +9,31 @@ import api.schemas.post as post_schema
 
 router = APIRouter()
 
-@router.get("/users/{user_id}/post")
+@router.get("/users/{user_id}/post", response_model=post_schema.Post)
 async def list_posts():
+    return [post_schema.Post(id=1, contents="Test Contents")]
+
+@router.post("/users/{user_id}/post", response_model=post_schema.PostCreateResponse)
+async def create_posts(post_bod):
     pass
 
-@router.post("/users/{user_id}/post")
-async def create_posts():
-    pass
-
-@router.put("/users/{user_id}/post/{post_id}")
+@router.put("/users/{user_id}/post/{post_id}", response_model=post_schema.PostCreateResponse)
 async def update_posts():
     pass
 
-@router.delete("/users/{user_id}/post/{post_id}")
+@router.delete("/users/{user_id}/post/{post_id}", response_model=None)
 async def delete_posts():
     pass
+
+
+@router.post("/users", response_model=user_schema.UserCreateResponse)
+async def create_users(user_body: user_schema.UserCreate):
+    return user_schema.UserCreateResponse(id=1, **user_body.dict())
+
+@router.put("/users/{user_id}", response_model=user_schema.UserCreateResponse)
+async def update_users(user_id: int, user_body: user_schema.UserCreate):
+    return user_schema.UserCreateResponse(id=user_id, **user_body.dict())
+
+@router.delete("/users/{user_id}", response_model=None)
+async def delete_users(user_id: int):
+    return
--- FastAPI/api/schemas/post.py
+++ FastAPI/api/schemas/post.py
@@ -2,9 +2,25 @@ from typing import Optional
 
 from pydantic import BaseModel, ConfigDict, Field
 
-class Post(BaseModel):
-    id: int
+class PostBase(BaseModel):
     contents: Optional[str] = Field(None)
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "contents": "None",
+                }
+            ]
+        }
+    }
 
-class PostCreate(BaseModel):
+class Post(PostBase):
     id: int
+    model_config = ConfigDict(from_attributes=True)
+
+class PostCreate(PostBase):
+    pass
+
+class PostCreateResponse(PostCreate):
+    id: int
+    model_config = ConfigDict(from_attributes=True)
