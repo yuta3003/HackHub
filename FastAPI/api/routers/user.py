6b9342521c2e6f081b62b646f8de13c19acfa514
yuta3003 <y.nakazawa3003+github@gmail.com>
2024-01-21 00:01:00 +0900

    :sparkles: add password_hash

--- FastAPI/api/routers/user.py
+++ FastAPI/api/routers/user.py
@@ -2,6 +2,7 @@ from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+import hashlib
 
 import api.cruds.user as user_crud
 import api.schemas.user as user_schema
@@ -19,7 +20,10 @@ async def list_users(db: AsyncSession = Depends(get_db)):
 async def create_users(
     user_body: user_schema.UserCreate, db: AsyncSession = Depends(get_db)
 ):
-    return await user_crud.create_user(db=db, user_create=user_body)
+    user_create: user_schema.UserCreate
+    user_create = user_body
+    user_create.password_hash = hash_string(user_body.password_hash)
+    return await user_crud.create_user(db=db, user_create=user_create)
 
 
 @router.put("/users/{user_id}", response_model=user_schema.UserCreateResponse)
@@ -40,3 +44,10 @@ async def delete_users(user_id: int, db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
 
     return await user_crud.delete_user(db=db, original=user)
+
+def hash_string(input_string):
+    hash_object = hashlib.sha256()
+    hash_object.update(input_string.encode('utf-8'))
+    hashed_string = hash_object.hexdigest()
+
+    return hashed_string
