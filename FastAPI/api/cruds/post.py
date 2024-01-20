754ee52bac521c7902c8438392d4ce596dbca6bf
yuta3003 <y.nakazawa3003+github@gmail.com>
2024-01-20 22:16:30 +0900

    :sparkles: postsのCRUDを作成

--- FastAPI/api/cruds/post.py
+++ FastAPI/api/cruds/post.py
@@ -1,3 +1,7 @@
+from typing import List, Optional, Tuple
+
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.models.model as model
@@ -14,14 +18,43 @@ async def create_post(
     await db.refresh(post)
     return post
 
-async def read_post(db: AsyncSession) -> List[Tuple[int, int, str]]:
+async def read_post(user_id: int, db: AsyncSession) -> List[Tuple[int, int, str]]:
     result: Result = await (
         db.execute(
             select(
                 model.Post.post_id,
                 model.Post.user_id,
                 model.Post.contents,
+            ).filter(
+                model.Post.user_id == user_id
             )
         )
     )
     return result.all()
+
+async def get_user(db: AsyncSession, user_id: int) -> Optional[model.User]:
+    result: Result = await db.execute(
+        select(model.User).filter(model.User.user_id == user_id)
+    )
+    user: Optional[Tuple[model.User]] = result.first()
+    return user[0] if user else None  # 要素が一つであってもtupleで返却されるので１つ目の要素を取り出す
+
+async def get_post(db: AsyncSession, user_id: int, post_id: int) -> Optional[model.Post]:
+    result: Result = await db.execute(
+        select(model.Post).filter(model.Post.user_id == user_id, model.Post.post_id == post_id)
+    )
+    post: Optional[Tuple[model.Post]] = result.first()
+    return post[0] if post else None
+
+async def update_post(
+    db: AsyncSession, post_create: post_schema.PostCreate, original: model.Post
+) -> model.Post:
+    original.contents = post_create.contents
+    db.add(original)
+    await db.commit()
+    await db.refresh(original)
+    return original
+
+async def delete_post(db: AsyncSession, original: model.Post) -> None:
+    await db.delete(original)
+    await db.commit()
--- FastAPI/api/routers/post.py
+++ FastAPI/api/routers/post.py
@@ -10,19 +10,35 @@ from api.db import get_db
 router = APIRouter()
 
 @router.get("/users/{user_id}/posts", response_model=List[post_schema.Post])
-async def list_posts(user_id: int):
-    return [post_schema.Post(user_id=user_id, post_id=1, contents="Test!!")]
+async def list_posts(user_id: int, db: AsyncSession = Depends(get_db)):
+    # TODO: 関数を位置引数からキーワード引数に変更
+    return await post_crud.read_post(user_id, db)
 
 @router.post("/users/{user_id}/posts", response_model=post_schema.PostCreateResponse)
 async def create_posts(user_id: int, post_body: post_schema.PostCreate, db: AsyncSession = Depends(get_db)):
     # TODO: 指定されたuser_idが存在するか確認するようにする。
     return await post_crud.create_post(user_id, db, post_body)
-    # return post_schema.PostCreateResponse(user_id=user_id, post_id=1, **post_body.dict())
 
 @router.put("/users/{user_id}/posts/{post_id}", response_model=post_schema.PostCreateResponse)
-async def update_posts(user_id: int, post_id: int, post_body: post_schema.PostCreate):
-    return post_schema.PostCreateResponse(user_id=user_id, post_id=post_id, **post_body.dict())
+async def update_posts(user_id: int, post_id: int, post_body: post_schema.PostCreate, db: AsyncSession = Depends(get_db)):
+    user = await post_crud.get_user(db, user_id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    post = await post_crud.get_post(db, user_id=user_id, post_id=post_id)
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    return await post_crud.update_post(db, post_body, original=post)
 
 @router.delete("/users/{user_id}/posts/{post_id}", response_model=None)
-async def delete_posts(user_id: int, post_id: int):
-    return
+async def delete_posts(user_id: int, post_id: int, db: AsyncSession = Depends(get_db)):
+    user = await post_crud.get_user(db, user_id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    post = await post_crud.get_post(db, user_id=user_id, post_id=post_id)
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    return await post_crud.delete_post(db, original=post)
