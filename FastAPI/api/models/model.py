231b8c09ee5fab3f3a7534a5bec3765888515441
yuta3003 <y.nakazawa3003+github@gmail.com>
2024-01-21 16:09:03 +0900

    :tada: modelを修正

--- FastAPI/api/models/model.py
+++ FastAPI/api/models/model.py
@@ -8,7 +8,7 @@ class User(Base):
     __tablename__ = "users"
 
     user_id = Column(Integer, autoincrement=True, primary_key=True)
-    user_name = Column(String(1024), nullable=False)
+    user_name = Column(String(256), nullable=False, unique=True)
     password_hash = Column(String(256), nullable=False)
 
     post = relationship("Post", back_populates="user", cascade="delete")
