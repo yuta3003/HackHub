8fd6f3bb90d73c29227a5a8a112813411582d367
yuta3003 <y.nakazawa3003+github@gmail.com>
2024-01-23 14:08:05 +0900

    :bug: bug fix

--- FastAPI/api/schemas/user.py
+++ FastAPI/api/schemas/user.py
@@ -5,22 +5,10 @@ from pydantic import BaseModel, ConfigDict, Field
 
 class UserBase(BaseModel):
     user_name: Optional[str] = Field(None)
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "user_name": "anonymous",
-                    "password": "P@ssw0rd",
-                    "password_hash": "77cc538aa9b72abb040bb087d1358d58dec3f9b8e817de96af457807d083b5df",
-                }
-            ]
-        }
-    }
 
 
 class User(UserBase):
     user_id: int
-    # model_config = ConfigDict(from_attributes=True)
     model_config = {
         "json_schema_extra": {
             "examples": [
@@ -47,13 +35,12 @@ class UserCreateRequest(UserBase):
 
 class UserCreate(UserBase):
     password_hash: Optional[str] = Field(None)
-    # model_config = ConfigDict(from_attributes=True)
     model_config = {
         "json_schema_extra": {
             "examples": [
                 {
                     "user_name": "anonymous",
-                    "password_hash": "P@ssw0rd",
+                    "password_hash": "b03ddf3ca2e714a6548e7495e2a03f5e824eaac9837cd7f159c67b90fb4b7342",
                 }
             ]
         }
@@ -62,12 +49,11 @@ class UserCreate(UserBase):
 
 class UserCreateResponse(UserCreate):
     user_id: int
-    # model_config = ConfigDict(from_attributes=True)
     model_config = {
         "json_schema_extra": {
             "examples": [
                 {
-                    "user_id": int,
+                    "user_id": 1,
                     "user_name": "anonymous",
                     "password_hash": "b03ddf3ca2e714a6548e7495e2a03f5e824eaac9837cd7f159c67b90fb4b7342",
                 }
