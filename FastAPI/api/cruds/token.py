74442fead433a5d7e291536da0fcd8770facfaa0
yuta3003 <y.nakazawa3003+github@gmail.com>
2024-01-25 16:23:01 +0900

    :+1: update

--- FastAPI/api/cruds/token.py
+++ FastAPI/api/cruds/token.py
@@ -8,9 +8,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.models.model as model
 import api.schemas.token as token_schema
-from api.db import get_db
 import api.schemas.user as user_schema
-
+from api.db import get_db
 from api.schemas.oauth2 import oauth2_scheme
 
 SECRET_KEY = "your-secret-key"
@@ -25,26 +24,12 @@ def create_access_token(data: dict) -> str:
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-# ユーザー情報を用いてユーザーDBから該当ユーザーを検索する
-# def get_user(db, username:str):
-#     if username in db:
-#         user_dict = db[username]
-#         return UserInDB(**user_dict)
 
-# async def get_user_by_name(db: AsyncSession, user_name: str) -> Optional[model.User]:
-#     result: Result = await db.execute(
-#         select(model.User).filter(model.User.user_name == user_name)
-#     )
-#     user: Optional[Tuple[model.User]] = result.first()
-#     return user[0] if user else None  # 要素が一つであってもtupleで返却されるので１つ目の要素を取り出す
-
-
-# リクエストヘッダからユーザ名を返却
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+async def decode_username_from_token(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"}
+        headers={"WWW-Authenticate": "Bearer"},
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -56,104 +41,3 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         raise credentials_exception
 
     return token_data.username
-
-    # user = get_user_by_name(db=Depends(get_db), user_name=token_data.username)
-
-    # if user is None:
-    #     raise credentials_exception
-    # return user
-
-
-# async def get_current_active_user(
-#     current_user: Annotated[model.User, Depends(get_current_user)]
-# ):
-#     # if current_user.disabled:
-#     #     raise HTTPException(status_code=400, detail="Inactive user")
-#     # print(current_user)
-#     return current_user
-
-# # ユーザー情報が有効かどうかを調べ、有効だったらユーザー情報を返す
-# async def get_current_active_user(
-#     current_user: Annotated[user_schema.User, Depends(get_current_user)]
-# ):
-#     # if current_user.disabled:
-#     #     raise HTTPException(status_code=400, detail="Inactive user")
-#     return current_user
-
-# async def get_user_by_name(db: AsyncSession, user_name: str) -> Optional[model.User]:
-#     result: Result = await db.execute(
-#         select(model.User).filter(model.User.user_name == user_name)
-#     )
-#     user: Optional[Tuple[model.User]] = result.first()
-#     return user[0] if user else None  # 要素が一つであってもtupleで返却されるので１つ目の要素を取り出す
-
-
-# ユーザー認証関数
-# def get_current_user(token: str = Depends(oauth2_scheme)) -> token_schema.User:
-#     credentials_exception = HTTPException(
-#         status_code=401,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#     except JWTError:
-#         raise credentials_exception
-
-#     user = get_user_by_name(db, username)
-#     if user is None:
-#         raise credentials_exception
-#     return user
-
-# def verify_token(token: str):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         sub = payload.get("sub")
-#         if sub is None:
-#             raise credentials_exception
-#     except JWTError:
-#         raise credentials_exception
-
-#     # ここでトークンの有効期限などを追加の検証できます
-
-#     return sub
-
-
-# # リクエストヘッダに含まれるJWTからユーザー情報を取得し、該当するユーザーの情報を返す
-# async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"}
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = token_schema.TokenData(username=username)
-#     except JWTError:
-#         raise credentials_exception
-#     # user = get_user(fake_users_db, username=token_data.username)
-#     db: AsyncSession = get_db()
-#     user = get_user_by_name(db=db, user_name=token_data.username)
-#     if user is None:
-#         raise credentials_exception
-#     return user
-
-# # ユーザー情報が有効かどうかを調べ、有効だったらユーザー情報を返す
-# async def get_current_active_user(
-#     current_user: Annotated[model.User, Depends(get_current_user)]
-# ):
-#     # if current_user.disabled:
-#     #     raise HTTPException(status_code=400, detail="Inactive user")
-#     return current_user
--- FastAPI/api/routers/token.py
+++ FastAPI/api/routers/token.py
@@ -2,8 +2,7 @@ from typing import Annotated, List
 
 import pymysql
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-from fastapi.security import HTTPBearer
+from fastapi.security import HTTPBearer, OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.token as token_crud
@@ -13,21 +12,9 @@ import api.schemas.user as user_schema
 from api.db import get_db
 from api.utils.hash_generator import HashGenerator
 
-
-# from api.schemas.oauth2 import oauth2_scheme
-
 router = APIRouter()
 bearer_scheme = HTTPBearer()
 
-# # JWTの設定を定義
-# class Settings(BaseModel):
-#     authjwt_secret_key: str = "your-secret-key"
-#     authjwt_token_location: set = {"headers"}
-#     authjwt_algorithm: str = "HS256"
-
-# @AuthJWT.load_config
-# def get_config():
-#     return Settings()
 
 # ログインエンドポイント
 @router.post("/token", response_model=token_schema.TokenResponse)
@@ -37,52 +24,16 @@ async def login_for_access_token(
     user = await user_crud.get_user_by_name(db=db, user_name=auth_info.username)
     password_hash = HashGenerator().hash_string(auth_info.password)
     if user.password_hash == password_hash:
-        access_token = token_crud.create_access_token(
-            data={"sub": auth_info.username}
-        )
+        access_token = token_crud.create_access_token(data={"sub": auth_info.username})
         return {
             "access_token": access_token,
             "token_type": "bearer",
         }
     raise HTTPException(status_code=401, detail="Incorrect username or password")
 
+
 @router.get("/protected", dependencies=[Depends(bearer_scheme)])
 async def protected_endpoint(
-    user_name: Annotated[str, Depends(token_crud.get_current_user)]
-    # current_user: Annotated[user_schema.User, Depends(token_crud.get_current_active_user)]
+    user_name: Annotated[str, Depends(token_crud.decode_username_from_token)]
 ):
-    # この部分にはトークンが検証されている状態での処理を書く
-    return {"message": "ok"}
-# def protected(Authorize: AuthJWT = Depends()):
-#     Authorize.jwt_required()
-#     current_user = Authorize.get_jwt_subject()
-#     return {"user": current_user}
-
-
-# @router.get("/protected_resource", tags=["protected"], response_model=dict)
-# async def get_protected_resource(token: str = Depends(oauth2_scheme)):
-#     sub = token_crud.verify_token(token)
-#     return {"message": f"You have access to this protected resource! (User: {sub})"}
-
-# @router.get("/users/me/items")
-# async def read_own_items(
-#     current_user: Annotated[user_schema.User, Depends(token_crud.get_current_active_user)]
-# ):
-#     return [{"item_id": "Foo", "owner": current_user.username}]
-
-# @router.get("/protected_resource", tags=["protected"], response_model=dict, dependencies=[Depends(bearer_scheme)])
-# async def get_protected_resource(
-#     current_user: str = Depends(token_crud.get_current_user),
-# ):
-#     # sub = token_crud.verify_token(token)
-#     return {"current_user": current_user}
-#     # return {"message": f"You have access to this protected resource! (User: {sub})"}
-
-# エンドポイントの例
-# @router.get("/protected_resource1", tags=["protected"], response_model=dict)
-# async def get_protected_resource(
-#     token: str = Depends(token_crud.get_current_user),
-# ):
-#     # tokenを使った認証や処理を行う
-#     # 例として、tokenをそのまま返す
-#     return {"token": token}
+    return {"message": user_name}
