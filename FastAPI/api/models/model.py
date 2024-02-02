1119512fa73cb19428303a4c997ff109f5e0062c
yuta3003 <y.nakazawa3003+github@gmail.com>
2024-02-02 20:48:40 +0900

    :+1: add models

--- FastAPI/api/models/model.py
+++ FastAPI/api/models/model.py
@@ -1,32 +1,14 @@
 """
-Database Models Module.
+api.db モジュール
 
-This module defines SQLAlchemy database models for user and post entities.
+このモジュールは SQLAlchemy を使用してデータベースモデルを定義します。
+以下は含まれるクラスの簡単な説明です:
 
-Classes:
-    - User: Represents the 'users' table in the database.
-    - Post: Represents the 'posts' table in the database.
+- User: ユーザー情報を表すデータベーステーブルのモデルクラス。
+- Post: 投稿情報を表すデータベーステーブルのモデルクラス。
+- Comment: コメント情報を表すデータベーステーブルのモデルクラス。
 
-Attributes:
-    - user_id (Column): Integer column representing the user ID (primary key).
-    - user_name (Column): String column representing the user name.
-    - password_hash (Column): String column representing the hashed password.
-    - post (relationship): One-to-many relationship between User and Post models.
-
-    - post_id (Column): Integer column representing the post ID (primary key).
-    - user_id (Column): Integer column representing the foreign key to the 'users' table.
-    - contents (Column): String column representing the contents of the post.
-    - user (relationship): Many-to-one relationship between Post and User models.
-
-Usage:
-    - Import the 'User' and 'Post' classes.
-    - Use these classes to interact with the database tables.
-
-Example:
-    from api.models.model import User, Post
-    from api.db import Base
-
-    # Your SQLAlchemy models are defined and can be used in database operations.
+これらのクラスはデータベース内の異なるテーブルを表し、それぞれのテーブルに対する関連性も定義されています。
 """
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
@@ -36,15 +18,15 @@ from api.db import Base
 
 class User(Base):
     """
-    Represents the 'users' table in the database.
+    ユーザー情報を表すデータベーステーブルのモデルクラスです。
 
     Attributes:
-        user_id (Column): Integer column representing the user ID (primary key).
-        user_name (Column): String column representing the user name.
-        password_hash (Column): String column representing the hashed password.
-        post (relationship): One-to-many relationship between User and Post models.
+        user_id (int): ユーザーの一意の識別子。
+        user_name (str): ユーザーの名前。
+        password_hash (str): ユーザーのパスワードのハッシュ値。
+        post (relationship): ユーザーが作成した投稿との関連性。
+        comment (relationship): ユーザーが作成したコメントとの関連性。
     """
-
     __tablename__ = "users"
 
     user_id = Column(Integer, autoincrement=True, primary_key=True)
@@ -52,19 +34,20 @@ class User(Base):
     password_hash = Column(String(256), nullable=False)
 
     post = relationship("Post", back_populates="user", cascade="delete")
+    comment = relationship("Comment", back_populates="user", cascade="delete")
 
 
 class Post(Base):
     """
-    Represents the 'posts' table in the database.
+    投稿情報を表すデータベーステーブルのモデルクラスです。
 
     Attributes:
-        post_id (Column): Integer column representing the post ID (primary key).
-        user_id (Column): Integer column representing the foreign key to the 'users' table.
-        contents (Column): String column representing the contents of the post.
-        user (relationship): Many-to-one relationship between Post and User models.
+        post_id (int): 投稿の一意の識別子。
+        user_id (int): 投稿を作成したユーザーの識別子。
+        contents (str): 投稿の内容。
+        user (relationship): 投稿を作成したユーザーとの関連性。
+        comment (relationship): 投稿に対するコメントとの関連性。
     """
-
     __tablename__ = "posts"
 
     post_id = Column(Integer, autoincrement=True, primary_key=True)
@@ -72,23 +55,27 @@ class Post(Base):
     contents = Column(String(256))
 
     user = relationship("User", back_populates="post")
+    comment = relationship("Comment", back_populates="post", cascade="delete")
 
 
-class Commnents(Base):
+class Comment(Base):
     """
-    Represents the 'posts' table in the database.
+    コメント情報を表すデータベーステーブルのモデルクラスです。
 
     Attributes:
-        post_id (Column): Integer column representing the post ID (primary key).
-        user_id (Column): Integer column representing the foreign key to the 'users' table.
-        contents (Column): String column representing the contents of the post.
-        user (relationship): Many-to-one relationship between Post and User models.
+        comment_id (int): コメントの一意の識別子。
+        user_id (int): コメントを作成したユーザーの識別子。
+        post_id (int): コメントが対象とする投稿の識別子。
+        contents (str): コメントの内容。
+        user (relationship): コメントを作成したユーザーとの関連性。
+        post (relationship): コメントが対象とする投稿との関連性。
     """
+    __tablename__ = "comments"
 
-    __tablename__ = "posts"
-
-    post_id = Column(Integer, autoincrement=True, primary_key=True)
+    comment_id = Column(Integer, autoincrement=True, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.user_id"))
+    post_id = Column(Integer, ForeignKey("posts.post_id"))
     contents = Column(String(256))
 
-    user = relationship("User", back_populates="post")
+    user = relationship("User", back_populates="comment")
+    post = relationship("Post", back_populates="comment")
