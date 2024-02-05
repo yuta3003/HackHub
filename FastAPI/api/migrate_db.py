84104f87086de1222fbdeccbcdf4cfa305b4cb39
yuta3003 <y.nakazawa3003+github@gmail.com>
2024-02-05 18:42:34 +0900

    :bug: fix

--- FastAPI/api/migrate_db.py
+++ FastAPI/api/migrate_db.py
@@ -1,18 +1,3 @@
-"""
-Database Initialization Module.
-
-This module provides functionality for initializing the database using SQLAlchemy.
-It includes a function `reset_database` to drop and recreate all tables defined in the models.
-
-Usage:
-    - When executed as the main script, it resets the database.
-
-Note:
-    Make sure to update the `DB_URL` variable with the appropriate database connection URL.
-
-Example:
-    python migrate_db.py
-"""
 from sqlalchemy import create_engine
 
 from api.models.model import Base
@@ -23,16 +8,16 @@ engine = create_engine(DB_URL, echo=True)
 
 def reset_database():
     """
-    Reset the database by dropping and recreating all tables.
+    データベースをリセットする関数。
 
-    This function uses the SQLAlchemy Base.metadata object to drop and create tables
-    defined in the models.
+    この関数は現在のデータベースのテーブルを全て削除し、
+    Base.metadataに定義されているテーブルを再作成します。
 
-    Usage:
-        Call this function to reset the database.
+    Args:
+        なし
 
-    Example:
-        reset_database()
+    Returns:
+        なし
     """
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
